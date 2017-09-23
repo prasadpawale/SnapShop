f6b0abbeb95d491d92bbe21aa0a7e590caccb105
prasad.pawale <prasad.pawale@SEZ-DESK-42>
2017-09-23 12:49:09 +0530

    Upload control added

--- SnapShop.Design/SnapShop.Design/SnapShop.Design/Controllers/HomeController.cs
+++ SnapShop.Design/SnapShop.Design/SnapShop.Design/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.IO;
 using System.Web.Mvc;
 
 namespace SnapShop.Design.Controllers
@@ -31,6 +32,47 @@ namespace SnapShop.Design.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            foreach (string file1 in Request.Files)
+            {
+                var postedFile = Request.Files[file1];
+                string fileUploadedPath = Server.MapPath("~/UploadedFiles/") + Path.GetFileName(postedFile.FileName);
+                postedFile.SaveAs(Server.MapPath("~/UploadedFiles/") + Path.GetFileName(postedFile.FileName));
+                //Run(fileUploadedPath).Wait();
+
+            }
+
+
+
+            //if (file != null && file.ContentLength > 0) try
+            //    {
+            //        string path = Path.Combine(Server.MapPath("~/Files"),
+            //        Path.GetFileName(file.FileName));
+            //        file.SaveAs(path);
+            //        return Json(new
+            //        {
+            //            Error = false,
+            //            Message = "File Uploaded Successfully..!!!",
+            //            FilePath = file.FileName
+            //        }, JsonRequestBehavior.AllowGet);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        return Json(new
+            //        {
+            //            Error = false,
+            //            Message = "File Not Uploaded"
+            //        }, JsonRequestBehavior.AllowGet);
+            //    }
+            //else
+            //{
+            //    ViewBag.Message = "You have not specified a file.";
+            //}
             return View();
         }
     }
